41c793dfbf0b68ca7c395031111f780a12788d4d
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-24 07:42:06 +0530

    Removed unused item from imports

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HiMenuAlt4, hiMenyAlt4, HiX } from 'react-icons/hi';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Navbar.scss';
