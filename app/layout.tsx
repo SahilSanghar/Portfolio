db24d90d79d55a1ea8d88a245ca8b7f53547c5c5
SAHIL sanghar <sangharsahil546@gmail.com>
2024-07-15 17:48:26 +0530

    ...

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sahil Sanghar",
+  description: "Personal Portfolio Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className='dark'>
+      <head>
+        <link rel="icon" href="/WebLogo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,197 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+];
+
+    export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+    {
+        id: 5,
+        title: "Currently building a JS Animation library",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+    export const projects = [
+    {
+        id: 1,
+        title: "WanderLust",
+        des: "Hotel Booking Website",
+        img: "/p1.png",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+        link: "https://wanderlust-pf8r.onrender.com/listings",
+    },
+    {
+        id: 2,
+        title: "Meet Point",
+        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+        img: "/p2.svg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "/ui.yoom.com",
+    },
+    {
+        id: 3,
+        title: "Gold's Gym",
+        des: "Gym Management.",
+        img: "/p3.png",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+        link: "/ui.aiimg.com",
+    },
+    {
+        id: 4,
+        title: "ZeeCare",
+        des: "Hospital Operation Scheduler.",
+        img: "/p4.png",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+        link: "/ui.apple.com",
+    },
+];
+
+    export const testimonials = [
+    {
+        quote:
+        "I'm delighted to have Sahil join PaceDream as a Full Stack Developer. His enthusiasm and understanding of our project requirements are impressive. Despite college commitments, Sahil is proactive and dedicated, showcasing a strong work ethic. He's already made a positive impression and I'm confident his skills will be a valuable asset to our team. We look forward to a successful journey together.",
+        name: "Chae Jimmy",
+        title: "Founder of PaceDream",
+        profile: "/profile1.png"
+    },
+    {
+        quote:
+        "I had the pleasure of working with Sahil Sanghar on multiple full stack development projects. His expertise in the MERN stack, Next.js, and Sass, along with his dedication and attention to detail, are outstanding. Sahil consistently met deadlines and communicated effectively, delivering high-quality, user-friendly applications. In particular, his work on an online banking application with advanced features like user authentication and secure OTP transactions was exemplary. Sahil's technical skills and professionalism make him a valuable asset to any team.",
+        name: "Wesley Brewer",
+        title: "Director of Chase Pay",
+        profile: "/profile2.png"
+    },
+];
+
+    export const companies = [
+    {
+        id: 1,
+        name: "PaceDream",
+        img: "/pacedream.svg",
+        company: "PaceDream",
+    },
+    {
+        id: 2,
+        name: "Chase Pay",
+        img: "/Chasepay.png",
+        company: "Chasepay",
+    },
+    {
+        id: 3,
+        name: "Unified Mentor",
+        img: "/UnifiedMentor.png",
+        company: "Unified Mentor",
+    },
+    {
+        id: 4,
+        name: "Afame Technology",
+        img: "/Afame.png",
+        company: "Afame Technology",
+    },
+];
+
+    export const workExperience = [
+    {
+        id: 1,
+        title: "Web Development Intern",
+        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity at Unifiend Mentor and Afame Technology.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Full Stack Web Developer",
+        desc: "Designed and developed Web app for both iOS & Android platforms with the help of PaceDream team.",
+        className: "md:col-span-2",
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "Freelance Web Dev Project",
+        desc: "Led the dev of a web app for a client, from initial concept to deployment using latest technologies.",
+        className: "md:col-span-2",
+        thumbnail: "/exp3.svg",
+    },
+    {
+        id: 4,
+        title: "Lead MERN Stack Developer",
+        desc: "Developed and maintained user-facing features using MERN technologies.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+    },
+];
+
+    export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/SahilSanghar",
+    },
+    {
+        id: 2,
+        img: "/insta.svg",
+        link: "https://www.instagram.com/thats.__.sahil",
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/sahilsanghar",
+    },
+];
