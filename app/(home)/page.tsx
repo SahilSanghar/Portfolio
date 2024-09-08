34ae6da27684f4e72757b9ad69af7989afc8408a
SAHIL sanghar <sangharsahil546@gmail.com>
2024-09-08 11:49:55 +0530

    ...

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
-export default function Home() {
-return (
+import React, { ReactNode } from 'react';
+
+export default function Home({ children }: { children: ReactNode }) {
+  return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-        <div className="max-w-7xl w-full">
+      <div className="max-w-7xl w-full">
         {children}
-        </div>
+      </div>
     </main>
-);
+  );
 }
