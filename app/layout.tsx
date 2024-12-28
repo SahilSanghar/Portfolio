d772a17b56456b0512e12d908cbb43be599bfca8
Sahil Sanghar <sangharsahil546@gmail.com>
2024-12-28 06:52:51 +0530

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,39 @@ import { Providers } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sahil Sanghar",
-  description: "Personal Portfolio Website",
+  title: "Sahil Sanghar | Freelance Web Developer & UI/UX Designing Specialist",
+  description: "Personal portfolio showcasing the work of Sahil Sanghar, a skilled freelance web developer and UI/UX designer.",
+
+  openGraph: {
+    title: "Sahil Sanghar | Freelance Web Developer & UI/UX Designing Specialist",
+    description: "I am a skilled freelance website developer and UI/UX designer. Specializing in creating custom, responsive websites and applications that deliver exceptional user experiences and drive business success."
+,
+    images: [
+      {
+        url: "https://www.sahilsanghar.com/WebLogo.png", // Image URL
+        width: 1200,
+        height: 630,
+        alt: "Sahil Sanghar - Freelance Web Developer & UI/UX Designing Specialist"
+      }
+    ],
+    url: "https://www.sahilsanghar.com/",
+    type: "website",
+  },
+
+  // Twitter metadata
+  twitter: {
+    card: "summary_large_image",
+    title: "Sahil Sanghar | Freelance Web Developer & UI/UX Designing Specialist",
+    description: "Discover the portfolio of Sahil Sanghar, a skilled freelance web developer and UI/UX designer. Specializing in creating custom, responsive websites and applications that deliver exceptional user experiences and drive business success.",
+    images: [
+      {
+        url: "https://www.sahilsanghar.com/WebLogo.png", // Image URL
+        width: 1200,
+        height: 630,
+        alt: "Sahil Sanghar - Freelance Web Developer & UI/UX Designing Specialist"
+      }
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://www.sahilsanghar.com',
+    generateRobotsTxt: true, // Will generate robots.txt automatically
+    sitemapSize: 7000,
+    changefreq: 'daily',
+    priority: 0.7,
+    // Optional configurations, you can customize more if needed
+}
