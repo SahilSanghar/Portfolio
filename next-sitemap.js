7e81bf80163276bb5cb4e22a048b4d60ee271e21
Sahil Sanghar <sangharsahil546@gmail.com>
2025-06-21 14:40:06 +0530

    ...

--- next-sitemap.js
+++
@@ -1,9 +0,0 @@
-// next-sitemap.js
-module.exports = {
-    siteUrl: 'https://www.sahilsanghar.com',
-    generateRobotsTxt: true, // Will generate robots.txt automatically
-    sitemapSize: 7000,
-    changefreq: 'daily',
-    priority: 0.7,
-    // Optional configurations, you can customize more if needed
-}
